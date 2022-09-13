b4fdd62ad3f29e69745719369217312623259f3d
Vivian Hua <vivianhua@Vivians-MacBook-Pro.local>
2022-09-13 11:46:22 -0700

    add react router

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { About } from './Pages/About';
+import { Landing } from './Pages/Landing';
+import { Portfolio } from './Pages/Portfolio';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />}>
+          <Route index element={<Landing />} />
+          <Route path="blogs" element={<About />} />
+          <Route path="contact" element={<Portfolio />} />
+        </Route>
+      </Routes>
+     </BrowserRouter>
     </div>
   );
 }
