830e19637292521e6a02eba2ef214347684e9264
Vivian Hua <vivianhua@Vivians-MacBook-Pro.local>
2022-09-15 17:12:27 -0700

    style nav menu

--- app/src/Pages/Navigation.tsx
+++ app/src/Pages/Navigation.tsx
@@ -1,12 +1,21 @@
 import { Outlet, Link } from "react-router-dom";
+import "./Navigation.css";
 
 export function Navigation() {
-    return <>
-    <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/portfolio">Portfolio</Link>
-    </nav>
-    <Outlet />
-  </>;
+  return (
+    <div className="nav-menu">
+      <ul className="nav-items">
+        <li className="nav-item">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/about">About</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/portfolio">Portfolio</Link>
+        </li>
+      </ul>
+      <Outlet />
+    </div>
+  );
 }
