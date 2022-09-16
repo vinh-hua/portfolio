123c7ec75910c9f413ba31d3f9bcf136d0cb746a
Vivian Hua <vivianhua@Vivians-MacBook-Pro.local>
2022-09-15 18:25:26 -0700

    more skeleton

--- app/src/Pages/About.tsx
+++ app/src/Pages/About.tsx
@@ -2,7 +2,8 @@ export function About() {
   return (
     <>
       <h1>about me</h1>
-      <p>blah blah blah</p>
+      <p>what i'm up to right now...</p>
+      <p>blah blah blah books, bouldering, film photos</p>
     </>
   );
 }
--- app/src/Pages/Portfolio.tsx
+++ app/src/Pages/Portfolio.tsx
@@ -2,9 +2,9 @@ export function Portfolio() {
   return (
     <>
       <h1>portfolio</h1>
-      <p>microsoft viva insights</p>
+      <p>microsoft - viva insights</p>
       <p>capstone - civic qa</p>
-      <p>tommi</p>
+      <p>new tech - tommi</p>
     </>
   );
 }
